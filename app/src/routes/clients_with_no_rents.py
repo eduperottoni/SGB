80f7d7da82993cc71eedde69ee94c32844a1f7e6
Eduardo Perottoni <edu.perottoni@gmail.com>
2023-11-27 20:40:39 -0300

    Modifying special query so it can return 2 columns

--- app/src/routes/clients_with_no_rents.py
+++ app/src/routes/clients_with_no_rents.py
@@ -4,15 +4,15 @@ from db_utils.utils import execute_query
 
 def get_clients_with_0_rents():
     query = """
-    SELECT Cliente.nome AS cliente, COUNT(Historico.cliente) AS num_alugueis 
+    SELECT Cliente.nome AS cliente, Cliente.data_registro AS data_registro
     FROM Cliente 
     LEFT JOIN Historico ON Cliente.cpf = Historico.cliente 
     WHERE Historico.cliente IS NULL 
-    GROUP BY Cliente.nome
+    GROUP BY Cliente.nome, Cliente.data_registro
     """
     tuples = execute_query(query)
     logging.debug(tuples)
-    keys_to_consider=['cliente']
+    keys_to_consider=['cliente', 'data_registro']
     return render_template('special_queries_response.html', 
                            response_list=tuples, 
                            keys_to_consider=keys_to_consider,
